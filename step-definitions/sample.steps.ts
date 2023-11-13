a4c02db251dbabcf5f480ef823806a0cbbbd9f4f
anujkchhetri22 <150718982+anujkchhetri22@users.noreply.github.com>
2023-11-13 13:52:31 -0600

    Update sample.steps.ts

--- step-definitions/sample.steps.ts
+++ step-definitions/sample.steps.ts
@@ -1,26 +1,41 @@
 import { Given, When, Then } from 'cucumber';
-import { browser, $, $$ } from 'webdriverio';
+import { browser, $, $$, expect } from 'webdriverio';
 
 Given('I am on the website', async () => {
   await browser.url('https://magento.softwaretestingboard.com/');
 });
 
 When('I access the Shop', async () => {
-  // Perform actions to access the shop
+  await $('a[href="/shop"]').click();
+  await expect(browser).toHaveUrlContaining('/shop');
 });
 
 When('I filter for a Certain Category', async () => {
-  // Perform actions to filter for a certain category
+  // Assuming the category filter is identified by the CSS selector 'input.category-filter'
+  await $('input.category-filter').setValue('JavaScript');
+  await $('button.category-filter-submit').click();
+  // Add an assertion to check if the category is applied successfully
+  await expect($('span.category-title')).toHaveText('JavaScript');
 });
 
 When('I open the Product Page', async () => {
-  // Perform actions to open the product page
+  // Assuming the product link is identified by the CSS selector 'a.product-link'
+  await $$('a.product-link')[0].click();
+  // Add an assertion to check if the product page is loaded successfully
+  await expect(browser).toHaveUrlContaining('/product');
 });
 
 When('I add multiple quantities of the product to the cart', async () => {
-  // Perform actions to add multiple quantities to the cart
+  // Assuming the quantity input is identified by the CSS selector 'input.quantity'
+  await $('input.quantity').setValue(2);
+  await $('button.add-to-cart').click();
+  // Add an assertion to check if the product is added to the cart successfully
+  await expect($('span.cart-quantity')).toHaveText('2');
 });
 
 Then('I perform a Checkout', async () => {
-  // Perform actions to complete the checkout
+  // Assuming the checkout button is identified by the CSS selector 'button.checkout'
+  await $('button.checkout').click();
+  // Add assertions or further actions to complete the checkout process
+  // For example, you might want to check if the checkout page is loaded
 });
